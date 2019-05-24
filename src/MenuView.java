7f67103c85032dcfc2a1ffdb973aa6e201bec722
Justin <Tracyjn@mail.uc.edu>
2019-05-23 21:42:21 -0400

    Created window

--- src/MenuView.java
+++ src/MenuView.java
@@ -1,3 +1,23 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class MenuView
 {
+    private Stage menuStage;
+    private AnchorPane menuPane;
+    private Scene menuScene;
+
+    public MenuView()
+    {
+        menuStage = new Stage();
+        menuStage.setTitle("Pong");
+        menuStage.setMaximized(true);
+        menuStage.setResizable(false);
+        menuPane = new AnchorPane();
+        menuPane.setStyle("-fx-background-color: BLACK;");
+        menuScene = new Scene(menuPane);
+        menuStage.setScene(menuScene);
+        menuStage.show();
+    }
 }
